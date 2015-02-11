cfb7acffc1772c44fee76eeba80f666b60268b92
kouichi <kouichi@kiritsubo.(none)>
2015-02-11 14:31:50 +0900

    installed texlive and it worked, but xdvi does not work with japanese font

---
+++ 0211-2015-kiritsubo.sh
@@ -0,0 +1,26 @@
+#! /bin/bash
+
+# ref: http://www.mk-mode.com/octopress/2012/09/15/15002016/
+
+TMP_DIR=tmp-`date +%m%d-%Y`
+# if [ -n $TMP_DIR ]; then
+#   mkdir $TMP_DIR
+# else
+#   echo "TMP_DIR exists"
+# fi
+
+cd $TMP_DIR
+# wget  wget http://mirror.ctan.org/systems/texlive/tlnet/install-tl-unx.tar.gz
+# tar -xzvf install-tl-unx.tar.gz
+# cd install-tl-20150210
+# sudo ./install-tl
+
+# sudo yum install xorg-x11-xauth
+
+# sudo emacs /etc/ssh/sshd_config
+# set
+# X11Forwarding yes
+# X11DisplayOffset 10
+# X11UseLocalhost yes
+
+# sudo service sshd restart
