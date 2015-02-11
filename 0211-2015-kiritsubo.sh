9aa66c434cbd734d92e6a198635b25dc4e88ecca
kouichi <kouichi@kiritsubo.(none)>
2015-02-11 15:03:22 +0900

    added config for yatex

--- 0211-2015-kiritsubo.sh
+++ 0211-2015-kiritsubo.sh
@@ -3,11 +3,11 @@
 # ref: http://www.mk-mode.com/octopress/2012/09/15/15002016/
 
 TMP_DIR=tmp-`date +%m%d-%Y`
-# if [ -n $TMP_DIR ]; then
-#   mkdir $TMP_DIR
-# else
-#   echo "TMP_DIR exists"
-# fi
+if [ ! -e $TMP_DIR ]; then
+  mkdir $TMP_DIR
+else
+  echo "TMP_DIR exists"
+fi
 
 cd $TMP_DIR
 # wget  wget http://mirror.ctan.org/systems/texlive/tlnet/install-tl-unx.tar.gz
@@ -26,3 +26,19 @@ cd $TMP_DIR
 # sudo service sshd restart
 
 # echo 'PATH=$PATH:/usr/local/texlive/2014/bin/x86_64-linux' >> ~/.zshrc
+
+# YaTeX setting
+# wget https://www.yatex.org/yatex1.78.4.tar.gz
+# tar xzf yatex1.78.4.tar.gz
+cd yatex1.78.4
+# make elc
+# sudo make install
+
+# edit .emacs
+# (setq load-path (cons (expand-file-name "/usr/share/emacs/site-lisp/yatex") load-path))
+# (setq auto-mode-alist
+#       (cons (cons "\\.tex$" 'yatex-mode) auto-mode-alist))
+# (autoload 'yatex-mode "yatex" "Yet Another LaTeX mode" t)
+
+cd ~/hoge/works
+rm -r $TMP_DIR
