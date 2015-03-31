d04ce73cb1846dd9165324825bea762f152c6657
raspberry <raspberry.pythagora@gmail.com>
2015-03-31 14:56:28 +0000

    modify: added directions for pi2

--- 0331-2015-raspberrypi.sh
+++ 0331-2015-raspberrypi.sh
@@ -1,10 +1,13 @@
 #! /bin/bash
 
+# install webiopi
+
 # mkdir tmp
 cd tmp
 # wget http://sourceforge.net/projects/webiopi/files/WebIOPi-0.7.1.tar.gz
 # tar -xvzf WebIOPi-0.7.1.tar.gz
 cd WebIOPi-0.7.1
 
+# follow http://code.google.com/p/webiopi/issues/detail?id=140
 
 rm -rf tmp
