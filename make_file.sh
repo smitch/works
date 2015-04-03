79793f3d6fe4a1d3454b8216e089f2e99a5dfd01
smitch <kouichi.sumizaki@gmail.com>
2015-04-03 11:52:33 +0900

    modified: make_file.sh           added template

--- make_file.sh
+++ make_file.sh
@@ -1,7 +1,20 @@
 #! /bin/bash
 
 FILE_NAME=`date +%m%d-%Y`-`hostname`
-touch $FILE_NAME.sh
+echo make file: $FILE_NAME.sh
+cat <<EOF > $FILE_NAME.sh
+#! /bin/bash
+TMP_DIR=tmp-$FILE_NAME
+if [ ! -e \$TMP_DIR ]; then
+  mkdir \$TMP_DIR
+fi
+
+# rm -rf \$TMP_DIR
+
+EOF
+
+echo change permission:$FILE_NAME.sh u+x 
 chmod u+x $FILE_NAME.sh
 
+echo git add: $FILE_NAME.sh
 git add $FILE_NAME.sh
