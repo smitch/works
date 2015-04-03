7553154db1f2adbd5738162c6ae81c6704c6e6b8
smitch <kouichi.sumizaki@gmail.com>
2015-04-03 12:29:44 +0900

    postfix setup not finished

--- 0403-2015-kiritsubo.sh
+++ 0403-2015-kiritsubo.sh
@@ -1,13 +1,18 @@
 #! /bin/bash
 
-# contents: postfix setup
+# contents: postfix setup -> not finished
 
 TMP_DIR=tmp-0403-2015-kiritsubo
 if [ ! -e $TMP_DIR ]; then
   mkdir $TMP_DIR
 fi
-
 cd $TMP_DIR
+
+# sudo yum install postfix
+# sudo service postfix start
+
+# echo edit/etc/postfix/main.cf
+# inet_interfaces = $myhostname, localhost
 
 # rm -rf $TMP_DIR
 
