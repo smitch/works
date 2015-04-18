69881de5c4c47a371483c0b1991555f1ca9d6846
smitch <kouichi.sumizaki@gmail.com>
2015-04-18 09:53:48 +0900

    installed chromium-browser and emacs key binding on gtk

--- 0418-2015-akashi.sh
+++ 0418-2015-akashi.sh
@@ -1,11 +1,16 @@
 #! /bin/bash
 
-# contents:
+# contents: install chromium browser
+# make cntl-h backspace ref:https://wiki.ubuntulinux.jp/UbuntuTips/Desktop/GnomeEmacsKeyTheme
 
-TMP_DIR=tmp-0418-2015-akashi
-if [ ! -e $TMP_DIR ]; then
-  mkdir $TMP_DIR
-fi
-cd $TMP_DIR
+# sudo apt-get install chromium-browser
+gsettings set org.gnome.desktop.interface gtk-key-theme Emacs
+
+
+# TMP_DIR=tmp-0418-2015-akashi
+# if [ ! -e $TMP_DIR ]; then
+#   mkdir $TMP_DIR
+# fi
+# cd $TMP_DIR
 # rm -rf $TMP_DIR
 
