284f59b6700b99176eff6ff86a4d2dceb991e92e
raspberry <raspberry.pythagora@gmail.com>
2015-04-19 13:51:44 +0000

    set webiopi start default

--- 0331-2015-raspberrypi.sh
+++ 0331-2015-raspberrypi.sh
@@ -3,11 +3,13 @@
 # install webiopi http://code.google.com/p/webiopi/wiki/INSTALL
 
 # mkdir tmp
-cd tmp
+# cd tmp
 # wget http://sourceforge.net/projects/webiopi/files/WebIOPi-0.7.1.tar.gz
 # tar -xvzf WebIOPi-0.7.1.tar.gz
-cd WebIOPi-0.7.1
+# cd WebIOPi-0.7.1
 
 # follow http://code.google.com/p/webiopi/issues/detail?id=140
 
-rm -rf tmp
+# update-rc.d webiopi defaults
+
+# rm -rf tmp
