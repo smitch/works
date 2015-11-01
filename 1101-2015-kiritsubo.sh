a0401993ed9f9ed3e54b12cd3f690200b7cdb8b2
smitch <kouichi.sumizaki@gmail.com>
2015-11-01 20:23:48 +0900

    installed auto-install yasnippet ajc packages

--- 1101-2015-kiritsubo.sh
+++ 1101-2015-kiritsubo.sh
@@ -2,12 +2,15 @@
 
 # contents: install hatena-mode to emacs
 #           ref http://hatena-diary-el.osdn.jp/
+#
+#           install auto-complete-mode
 
-# TMP_DIR=tmp-1101-2015-kiritsubo
-# if [ ! -e $TMP_DIR ]; then
-#  mkdir $TMP_DIR
-# fi
-# cd $TMP_DIR
+
+TMP_DIR=tmp-1101-2015-kiritsubo
+if [ ! -e $TMP_DIR ]; then
+ mkdir $TMP_DIR
+fi
+cd $TMP_DIR
 
 echo 'download el files from https://osdn.jp/projects/hatena-diary-el/releases/'
 echo 'mv hatena-diary directory to .emacs.d'
@@ -20,6 +23,25 @@ add below to .emacs
 (setq hatena-usrid "<hatena user id>")
 (setq hatena-twitter-flag t)
 EOF
+
+# wget https://elpa.gnu.org/packages/cl-lib-0.5.el
+# emacs -batch -f batch-byte-compile cl-lib-0.5.el
+# mv cl-lib-0.5.elc ~/.emacs.d/cl-lib.elc
+
+echo "install auto-complete by auto-install"
+echo "M-x auto-install-batch"
+echo "select auto-comlete"
+
+echo "install yasnippet"
+git clone --recursive https://github.com/capitaomorte/yasnippet
+mv yasnippet ~/.emacs.d/yasnippet
+
+# echo "install yasnippet-config by auto-install"
+
+echo "install auto-java-complete"
+git clone https://github.com/emacs-java/auto-java-complete
+javac Tags.java
+java -cp "/usr/java/packages/lib/amd64:/usr/lib64:/lib64:/lib:/usr/lib/jvm/java-1.8.0-openjdk-1.8.0.25-3.b17.el6_6.x86_64:." Tags
 
 # cd ../
 # rm -rf $TMP_DIR
