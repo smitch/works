6b02ad2d493577b6ebe367f8875e76906f2f1a38
smitch <kouichi.sumizaki@gmail.com>
2015-12-19 20:58:57 +0900

    modified make_file.sh as directory hierarchy changed

--- make_file.sh
+++ make_file.sh
@@ -2,16 +2,30 @@
 
 FILE_NAME=`date +%m%d-%Y`-`hostname`
 echo make file: $FILE_NAME.sh
+HOSTNAME=`hostname`
+SCRIPTDIR=`dirname $0`
+cd $SCRIPTDIR
+
+if [ ! -e \$HOSTNAME ]; then
+ mkdir \$HOSTNAME
+fi
+cd $HOSTNAME
+
 cat <<EOF > $FILE_NAME.sh
 #! /bin/bash
 
 # contents:
 
-#TMP_DIR=tmp-$FILE_NAME
-#if [ ! -e \$TMP_DIR ]; then
-#  mkdir \$TMP_DIR
-#fi
-#cd \$TMP_DIR
+TMP_DIR=tmp-$FILE_NAME
+if [ ! -e \$TMP_DIR ]; then
+ mkdir \$TMP_DIR
+fi
+cd \$TMP_DIR
+
+# WRITE SCRIPT HERE
+
+# UNCOMMENT TO DELETE TMP DIR
+cd ../
 # rm -rf \$TMP_DIR
 
 EOF
