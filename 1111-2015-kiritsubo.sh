df224704930e981543c6bbbc220ab45da2b4738e
smitch <kouichi.sumizaki@gmail.com>
2015-12-19 19:20:43 +0900

    Revert "add tmp.sh and renamed files"
    This reverts commit 5d5305dd321ad67bb322a9d43ff4dd56abe64f3f.

---
+++ 1111-2015-kiritsubo.sh
@@ -0,0 +1,25 @@
+#! /bin/bash
+
+# contents:install magit to emacs
+
+echo M-x install-elisp
+echo http://tromey.com/elpa/package-install.el
+
+echo M-x package-list-packages
+echo find magit, type i and x
+
+echo (require magit) in .emacs
+
+# TMP_DIR=tmp-1111-2015-kiritsubo
+# if [ ! -e $TMP_DIR ]; then
+#  mkdir $TMP_DIR
+# fi
+# cd $TMP_DIR
+
+
+
+# cd magit
+
+# cd $TMP_DIR/..
+# rm -rf $TMP_DIR
+
