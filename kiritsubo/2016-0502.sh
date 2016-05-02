72a6f9392d9d789b3bbeefe59f6c33107089b24e
smitch <kouichi.sumizaki@gmail.com>
2016-05-02 16:31:48 +0900

    installed auto-install

--- kiritsubo/2016-0502.sh
+++ kiritsubo/2016-0502.sh
@@ -9,6 +9,22 @@
 # cd $TMP_DIR
 
 # WRITE SCRIPT HERE
+emacs_dir=~/.emacs.d
+
+if [ ! -e ${emacs_dir}/auto-install ]; then
+  mkdir -p ${emacs_dir}/auto-install
+fi
+
+# cd ${emacs_dir}/auto-install
+# wget 'http://www.emacswiki.org/emacs/download/auto-install.el'
+# emacs --batch -Q -f batch-byte-compile auto-install.el
+
+echo add .emacs below
+echo (setq load-path (cons (expand-file-name "~/.emacs.d/auto-install") load-path))
+echo (require 'auto-install)
+echo (setq load-path (cons auto-install-directory load-path))
+echo ;; (auto-install-update-emacswiki-package-name t)
+
 
 # UNCOMMENT TO DELETE TMP DIR
 # cd ../
