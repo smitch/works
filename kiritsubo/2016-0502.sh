95bdabaf9a6e2b6369930be2d7243dd34d1a60cb
smitch <kouichi.sumizaki@gmail.com>
2016-05-02 13:39:53 +0900

    created new work file

---
+++ kiritsubo/2016-0502.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+
+# contents: install emacs packages
+
+# TMP_DIR=tmp-2016-0502
+# if [ ! -e $TMP_DIR ]; then
+#  mkdir $TMP_DIR
+# fi
+# cd $TMP_DIR
+
+# WRITE SCRIPT HERE
+
+# UNCOMMENT TO DELETE TMP DIR
+# cd ../
+# rm -rf $TMP_DIR
+
