1be0423a1817da3a24e3ade4140fe98df6756fdb
smitch <kouichi.sumizaki@gmail.com>
2016-06-10 23:42:46 +0900

    iptables log setting

---
+++ kiritsubo/2016-0610.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+
+# contents: iptables log setting
+
+# ref http://oxynotes.com/?p=8432
+
+
+# TMP_DIR=tmp-2016-0610
+# if [ ! -e $TMP_DIR ]; then
+#  mkdir $TMP_DIR
+# fi
+# cd $TMP_DIR
+
+# WRITE SCRIPT HERE
+
+# UNCOMMENT TO DELETE TMP DIR
+# cd ../
+# rm -rf $TMP_DIR
+
