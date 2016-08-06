532d22e9216859aa4e13ee7fb2fb5688af683b64
smitch <kouichi.sumizaki@gmail.com>
2016-08-07 00:02:32 +0900

    add config to send mail when logging in via ssh

--- kiritsubo/2016-0806.sh
+++ kiritsubo/2016-0806.sh
@@ -1,6 +1,10 @@
 #! /bin/bash
 
-# contents:
+# contents: install postfix and dovecot. send mail when logging in via ssh
+# ref: http://vps-tora.com/tsubo/serversman/postfix/
+#      http://think-t.hatenablog.com/entry/20120930/p1
+#      http://blog.livedoor.jp/alpha1280/archives/51465825.html
+#      https://bacchi.me/linux/ssh-login-alert/
 
 # TMP_DIR=tmp-2016-0806
 # if [ ! -e $TMP_DIR ]; then
@@ -9,6 +13,17 @@
 # cd $TMP_DIR
 
 # WRITE SCRIPT HERE
+sudo yum -y install postfix
+
+echo edit files according to ref site
+
+sudo yum -y install dovecot
+
+echo iptables open port 25
+
+echo make /etc/ssh/sshrc file
+echo echo "\"$USER\" has logged in from $SSH_CLIENT at `date +"%Y/%m/%d %p %I:%M:%S"` " | mail -s "bacchi.me sshd login alert" -r <from_address> <to_address>
+
 
 # UNCOMMENT TO DELETE TMP DIR
 # cd ../
