edcff62291b09fe31255776e73b06adc519fe46d
smitch <kouichi.sumizaki@gmail.com>
2016-08-06 23:51:53 +0900

    installed postfix and dovecot. test succeeded

---
+++ kiritsubo/2016-0806.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+
+# contents:
+
+# TMP_DIR=tmp-2016-0806
+# if [ ! -e $TMP_DIR ]; then
+#  mkdir $TMP_DIR
+# fi
+# cd $TMP_DIR
+
+# WRITE SCRIPT HERE
+
+# UNCOMMENT TO DELETE TMP DIR
+# cd ../
+# rm -rf $TMP_DIR
+
